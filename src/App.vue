3fecc938456d14874089c7f11c9571ab1e1c7db9
SimasDei <simondeiman@gmail.com>
2019-06-23 20:12:38 +0300

    add lifecycle methods, export activities to seprate data file

--- src/App.vue
+++ src/App.vue
@@ -83,6 +83,8 @@
 
 <script>
 import ActivityItem from './components/ActivityItem';
+import { fetchActivities } from '@/api';
+
 export default {
   name: 'app',
   components: { ActivityItem },
@@ -102,30 +104,36 @@ export default {
         id: '-Aj34jknvncx98812',
       },
       activities: {
-        '1546968934': {
-          id: '1546968934',
-          title: 'Learn Vue.js',
-          notes: 'I started today and it was not good.',
-          progress: 0,
-          category: '1546969049',
-          createdAt: 1546969144391,
-          updatedAt: 1546969144391,
-        },
-        '1546969212': {
-          id: '1546969212',
-          title: 'Read Witcher Books',
-          notes: 'These books are super nice',
-          progress: 0,
-          category: '1546969049',
-          createdAt: 1546969144391,
-          updatedAt: 1546969144391,
-        },
       },
       categories: {
         '1546969049': { text: 'books' },
         '1546969225': { text: 'movies' },
       },
     };
+  },
+  beforeCreate () {
+    console.log('beforeCreate called!')
+  },
+  created () {
+    this.activities = fetchActivities()
+  },
+  beforeMount () {
+    console.log('beforeMount called!')
+  },
+  mounted () {
+    console.log('mounted called!')
+  },
+  beforeUpdate () {
+    console.log('beforeUpdate called!')
+  },
+  updated () {
+    console.log('updated called!')
+  },
+  beforeDestroy () {
+    console.log('beforeDestroy called!')
+  },
+  destroyed () {
+    console.log('destroyed called!')
   },
   methods: {
     toggleTextDisplay() {
